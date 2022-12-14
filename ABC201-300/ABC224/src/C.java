cff7778813be8a0e96bd93557912ffcfd5989dc1
k-negishi <56011598+k-negishi@users.noreply.github.com>
2022-12-14 18:57:12 +0900

    commit

---
+++ ABC201-300/ABC224/src/C.java
@@ -0,0 +1,116 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.*;
+
+public class C {
+    final static int MOD = 1000000007;
+
+    public static void main(String[] args) {
+        MyScanner sc = new MyScanner();
+
+        var n = sc.nextInt();
+        var matrix = sc.nextLongMatrix(n, 2);
+
+        int count = 0;
+
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (0 != (matrix[i][0] - matrix[k][0]) * (matrix[j][1] - matrix[k][1]) - (matrix[j][0] - matrix[k][0]) * (matrix[i][1] - matrix[k][1])) {
+                        count++;
+                    }
+
+                }
+            }
+        }
+
+        System.out.println(count);
+
+    }
+
+    static class MyScanner {
+        static Scanner sc = new Scanner(System.in);
+
+        String next() {
+            return sc.next();
+        }
+
+        String nextLine() {
+            return sc.nextLine();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(sc.next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        int[] nextIntArray(int n) {
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++) {
+                array[i] = nextInt();
+            }
+            return array;
+        }
+
+        double[] nextDoubleArray(int n) {
+            double[] array = new double[n];
+            for (int i = 0; i < n; i++) {
+                array[i] = nextDouble();
+            }
+            return array;
+        }
+
+        long[] nextLongArray(int n) {
+            long[] array = new long[n];
+            for (int i = 0; i < n; i++) {
+                array[i] = nextLong();
+            }
+            return array;
+        }
+
+        String[] nextLineArray(int n) {
+            String[] array = new String[n];
+            for (int i = 0; i < n; i++) {
+                array[i] = nextLine();
+            }
+            return array;
+        }
+
+        int[][] nextIntMatrix(int n, int m) {
+            int[][] matrix = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    matrix[i][j] = nextInt();
+                }
+            }
+            return matrix;
+        }
+
+        double[][] nextDoubleMatrix(int n, int m) {
+            double[][] matrix = new double[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    matrix[i][j] = nextDouble();
+                }
+            }
+            return matrix;
+        }
+
+        long[][] nextLongMatrix(int n, int m) {
+            long[][] matrix = new long[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    matrix[i][j] = nextLong();
+                }
+            }
+            return matrix;
+        }
+    }
+}
---
+++ ABC201-300/ABC280/src/B.java
@@ -0,0 +1,117 @@
+import java.util.*;
+
+public class B {
+    final static int MOD = 1000000007;
+
+    public static void main(String[] args) {
+        MyScanner sc = new MyScanner();
+
+        var n = sc.nextInt();
+        var s = sc.nextLongArray(n);
+
+        var answer = new StringBuilder();
+
+        long preValue = 0;
+        for (int i = 0; i < s.length; i++) {
+            if (i == 0) {
+                answer.append(s[i]).append(" ");
+                preValue = s[i];
+                continue;
+            }
+
+            var a = s[i] - preValue;
+            answer.append(a).append(" ");
+            preValue += a;
+        }
+
+        System.out.println(answer.toString());
+
+
+    }
+
+    static class MyScanner {
+        static Scanner sc = new Scanner(System.in);
+
+        String next() {
+            return sc.next();
+        }
+
+        String nextLine() {
+            return sc.nextLine();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(sc.next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        int[] nextIntArray(int n) {
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++) {
+                array[i] = nextInt();
+            }
+            return array;
+        }
+
+        double[] nextDoubleArray(int n) {
+            double[] array = new double[n];
+            for (int i = 0; i < n; i++) {
+                array[i] = nextDouble();
+            }
+            return array;
+        }
+
+        long[] nextLongArray(int n) {
+            long[] array = new long[n];
+            for (int i = 0; i < n; i++) {
+                array[i] = nextLong();
+            }
+            return array;
+        }
+
+        String[] nextLineArray(int n) {
+            String[] array = new String[n];
+            for (int i = 0; i < n; i++) {
+                array[i] = nextLine();
+            }
+            return array;
+        }
+
+        int[][] nextIntMatrix(int n, int m) {
+            int[][] matrix = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    matrix[i][j] = nextInt();
+                }
+            }
+            return matrix;
+        }
+
+        double[][] nextDoubleMatrix(int n, int m) {
+            double[][] matrix = new double[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    matrix[i][j] = nextDouble();
+                }
+            }
+            return matrix;
+        }
+
+        long[][] nextLongMatrix(int n, int m) {
+            long[][] matrix = new long[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    matrix[i][j] = nextLong();
+                }
+            }
+            return matrix;
+        }
+    }
+}
